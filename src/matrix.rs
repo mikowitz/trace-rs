ff3e8cee6fbc06cb7e4b57e16022f85e2c889720
Michael berkowitz <michael.berkowitz@gmail.com>
2024-09-30 22:39:56 -0400

    Multiplying Matrices

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,7 @@
+use crate::tuple::Tuple4;
+use std::ops::Mul;
+
+#[derive(Debug, PartialEq)]
 pub struct Matrix([f32; 16]);
 pub struct Matrix3x3([f32; 9]);
 pub struct Matrix2x2([f32; 4]);
@@ -9,6 +13,41 @@ impl Matrix {
 
     pub fn at(&self, row: usize, column: usize) -> f32 {
         self.0[row * 4 + column]
+    }
+
+    pub fn write(&mut self, row: usize, column: usize, value: f32) {
+        self.0[row * 4 + column] = value;
+    }
+}
+
+impl Mul<Matrix> for Matrix {
+    type Output = Matrix;
+    fn mul(self, rhs: Matrix) -> Self::Output {
+        let mut m = Matrix([0.; 16]);
+        for row in 0..4 {
+            for col in 0..4 {
+                let mut val = 0.;
+                for i in 0..4 {
+                    val += self.at(row, i) * rhs.at(i, col)
+                }
+                m.write(row, col, val);
+            }
+        }
+        m
+    }
+}
+
+impl Mul<Tuple4> for Matrix {
+    type Output = Tuple4;
+    fn mul(self, rhs: Tuple4) -> Self::Output {
+        let mut t = [0.; 4];
+        for row in 0..4 {
+            t[row] = self.at(row, 0) * rhs.0
+                + self.at(row, 1) * rhs.1
+                + self.at(row, 2) * rhs.2
+                + self.at(row, 3) * rhs.3;
+        }
+        Tuple4(t[0], t[1], t[2], t[3])
     }
 }
 
@@ -27,7 +66,10 @@ impl Matrix3x3 {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::test::approx::Approx;
+    use crate::{
+        test::approx::Approx,
+        tuple::{point, Tuple4},
+    };
 
     impl Approx for Matrix {
         fn approximate(&self, rhs: Self) -> bool {
@@ -74,7 +116,7 @@ mod tests {
     }
 
     #[test]
-    fn matrix_equality() {
+    fn equality() {
         let a = Matrix([
             1., 2., 3., 4., 5., 6., 7., 8., 9., 8., 7., 6., 5., 4., 3., 2.,
         ]);
@@ -86,7 +128,7 @@ mod tests {
     }
 
     #[test]
-    fn matrix_inequality() {
+    fn inequality() {
         let a = Matrix([
             1., 2., 3., 4., 5., 6., 7., 8., 9., 8., 7., 6., 5., 4., 3., 2.,
         ]);
@@ -95,5 +137,32 @@ mod tests {
         ]);
 
         assert!(!a.approximate(b));
+    }
+
+    #[test]
+    fn multiplying_matrices() {
+        let a = Matrix([
+            1., 2., 3., 4., 5., 6., 7., 8., 9., 8., 7., 6., 5., 4., 3., 2.,
+        ]);
+        let b = Matrix([
+            -2., 1., 2., 3., 3., 2., 1., -1., 4., 3., 6., 5., 1., 2., 7., 8.,
+        ]);
+
+        let expected = Matrix([
+            20., 22., 50., 48., 44., 54., 114., 108., 40., 58., 110., 102., 16., 26., 46., 42.,
+        ]);
+
+        let actual = a * b;
+        assert!(actual.approximate(expected));
+    }
+
+    #[test]
+    fn multiplying_a_matrix_by_a_tuple() {
+        let a = Matrix([
+            1., 2., 3., 4., 2., 4., 4., 2., 8., 6., 4., 1., 0., 0., 0., 1.,
+        ]);
+        let b = point(1., 2., 3.);
+
+        assert_eq!(a * b, point(18., 24., 33.));
     }
 }
