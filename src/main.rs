1db48f2a32ddef5ddf4ecde335b9b002e233f13e
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-10-19 14:51:32 -0400

    the vec3 class (#2)
    * The vec3 Class
    
    * :paperclip:

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 use indicatif::ProgressIterator;
 use itertools::Itertools;
+use trace_rs::color::Color;
 
 fn main() {
     let image_width = 256;
@@ -11,15 +12,12 @@ fn main() {
         .cartesian_product(0..image_width)
         .progress_count(image_width as u64 * image_height as u64)
         .map(|(y, x)| {
-            let r = 0.0;
-            let g = y as f32 / (image_height - 1) as f32;
-            let b = x as f32 / (image_width - 1) as f32;
-
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            format!("{ir} {ig} {ib}")
+            let color = Color::new(
+                0.0,
+                y as f32 / (image_height - 1) as f32,
+                x as f32 / (image_width - 1) as f32,
+            );
+            color.to_ppm()
         })
         .join("\n");
 
