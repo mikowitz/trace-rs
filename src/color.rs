4873107e0d6569709f9e5b43368e472fb7021c60
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-10-20 13:29:04 -0400

    Rays, a Simple Camera, and Background

--- src/color.rs
+++ src/color.rs
@@ -3,8 +3,18 @@ use crate::vec3::Vec3;
 pub type Color = Vec3;
 
 impl Color {
+    pub fn black() -> Self {
+        Color::new(0., 0., 0.)
+    }
+
+    pub fn white() -> Self {
+        Color::new(1., 1., 1.)
+    }
+
     pub fn to_ppm(&self) -> String {
-        let Vec3([r, g, b]) = self;
+        let r = self[0];
+        let g = self[1];
+        let b = self[2];
 
         let ir = (255.999 * r) as i32;
         let ig = (255.999 * g) as i32;
