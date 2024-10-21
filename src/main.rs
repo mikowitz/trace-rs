9e546d91173bc3f94049f75728880b4d34f15009
Michael berkowitz <michael.berkowitz@gmail.com>
2024-10-21 13:13:01 -0400

    :paperclip:

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use trace_rs::{camera::Camera, sphere::Sphere, vec3::Point3};
 
 fn main() {
-    let mut world = vec![];
-    world.push(Sphere::new(Point3::new(0., 0., -1.), 0.5));
-    world.push(Sphere::new(Point3::new(0., -100.5, -1.), 100.));
+    let world = vec![
+        Sphere::new(Point3::new(0., 0., -1.), 0.5),
+        Sphere::new(Point3::new(0., -100.5, -1.), 100.),
+    ];
 
     let mut camera = Camera::default();
     camera.aspect_ratio = 16. / 9.;
