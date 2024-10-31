cbcb154ac9427e4f25000b8ff0c2384862d79720
Michael berkowitz <michael.berkowitz@gmail.com>
2024-10-31 19:03:30 -0400

    Antialiasing

--- src/color.rs
+++ src/color.rs
@@ -1,7 +1,7 @@
 use glam::Vec3;
 
 pub fn to_ppm(color: Vec3) -> String {
-    let c = color * 255.999;
+    let c = color.clamp(Vec3::splat(0.000), Vec3::splat(0.999)) * 256.0;
 
     format!("{} {} {}", c.x as u8, c.y as u8, c.z as u8)
 }
