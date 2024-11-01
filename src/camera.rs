ab46d1f9e8b867c295fc49edec303fe9970b8f36
Michael berkowitz <michael.berkowitz@gmail.com>
2024-11-01 11:30:38 -0400

    Positionable Camera

--- src/camera.rs
+++ src/camera.rs
@@ -13,6 +13,11 @@ pub struct Camera {
     pub samples_per_pixel: usize,
     pub max_depth: usize,
 
+    pub vfov: f32,
+    pub lookfrom: Vec3,
+    pub lookat: Vec3,
+    pub vup: Vec3,
+
     image_height: u32,
     center: Vec3,
     pixel00_loc: Vec3,
@@ -77,22 +82,28 @@ impl Camera {
     fn initialize(&mut self) {
         self.image_height = ((self.image_width as f32 / self.aspect_ratio) as u32).max(1);
 
-        self.center = Vec3::ZERO;
+        self.center = self.lookfrom;
 
         self.pixel_samples_scale = (self.samples_per_pixel as f32).recip();
 
-        let focal_length = 1.;
-        let viewport_height = 2.;
+        let focal_length = (self.lookfrom - self.lookat).length();
+        let theta = self.vfov.to_radians();
+        let h = (theta / 2.).tan();
+        let viewport_height = 2. * h * focal_length;
         let viewport_width = viewport_height * (self.image_width as f32 / self.image_height as f32);
 
-        let viewport_u = Vec3::X * viewport_width;
-        let viewport_v = Vec3::Y * -viewport_height;
+        let w = (self.lookfrom - self.lookat).normalize();
+        let u = self.vup.cross(w).normalize();
+        let v = w.cross(u);
+
+        let viewport_u = u * viewport_width;
+        let viewport_v = -v * viewport_height;
 
         self.pixel_delta_u = viewport_u / self.image_width as f32;
         self.pixel_delta_v = viewport_v / self.image_height as f32;
 
         let viewport_upper_left =
-            self.center - Vec3::Z * focal_length - viewport_u / 2. - viewport_v / 2.;
+            self.center - w * focal_length - viewport_u / 2. - viewport_v / 2.;
 
         self.pixel00_loc = viewport_upper_left + 0.5 * (self.pixel_delta_u + self.pixel_delta_v);
     }
